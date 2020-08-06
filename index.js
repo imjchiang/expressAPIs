afb330bef00562776a1e7d40d838d15f839cae44
imjchiang <jchiang296@gmail.com>
2020-08-06 16:25:08 -0700

    [JS] initialize packages, implement express and axios [CSS] add css file [EJS] add ejs file

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+
+//using ejs as the view engine for rendering ejs files
+app.set("view engine", "ejs");
+//express using static to access CSS
+app.use(express.static("static"));
+
+app.get("/", function(req, res)
+{
+    let qs = 
+    {
+        params:
+        {
+            s: "star wars",
+            apikey: "8de3572f"
+        }
+    }
+
+    axios.get("http://www.omdbapi.com", qs)
+    .then(function(response)
+    {
+        console.log(response.data);
+    });
+});
+
+app.listen(3000);
